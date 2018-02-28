94feddfde61520184133888ef41522962cd86505
Greg Kapka <greg@kapka.co.uk>
2018-02-28 23:30:20 +0000

    Adding public folder for servers static images

--- app/etheraffle.js
+++ app/etheraffle.js
@@ -23,18 +23,7 @@ app.use('/ico/whitepaper',  (req,res) => {
 app.use('/ethrelief/whitepaper',  (req,res) => {
   res.sendFile((__dirname + '/whitepaper/ethReliefWhitePaper.pdf'))
 })
-app.use('/onepager/full',  (req,res) => {
-  res.sendFile((__dirname + '/onePagers/LOTOnePager.jpg'))
-})
-app.use('/onepager/features',  (req,res) => {
-  res.sendFile((__dirname + '/onePagers/LOTOnePagerFeatures.jpg'))
-})
-app.use('/onepager/ico',  (req,res) => {
-  res.sendFile((__dirname + '/onePagers/LOTOnePagerICO.jpg'))
-})
-app.use('/onepager/token',  (req,res) => {
-  res.sendFile((__dirname + '/onePagers/LOTOnePagerToken.jpg'))
-})
+app.use('/public', express.static(path.join(__dirname + '/public')));
 
 /* Requests to etheraffle.com/ico picks up the ico react app from this location */
 app.use('/ico', express.static('/home/gregkapka/ico/build/'))
