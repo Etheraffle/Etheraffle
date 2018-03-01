561e2a150837c400f7d77abd192e12295c8f9054
Greg Kapka <greg@kapka.co.uk>
2018-03-01 15:37:02 +0000

    Adding grunt automation & switching to minified/prefixed css.

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -1,16 +1,69 @@
-// Load Grunt
 module.exports = function (grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-  // Tasks
-	});
-	// Load Grunt plugins
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-postcss');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-	// Register Grunt tasks
-	grunt.registerTask('default', ['watch']);
+    // Tasks
+    sass: { // Begin Sass Plugin
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/styles',
+          src: ['**/*.scss'],
+          dest: 'css',
+          ext: '.css'
+        }]
+      }
+    },
+    postcss: { // Begin Post CSS Plugin
+      options: {
+        map: false,
+        processors: [
+      require('autoprefixer')({
+            browsers: ['last 2 versions']
+          })
+    ]
+      },
+      dist: {
+        src: 'src/styles/css/main.css'
+      }
+    },
+    cssmin: { // Begin CSS Minify Plugin
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'src/styles/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    /*
+    uglify: { // Begin JS Uglify Plugin
+      build: {
+        src: ['src/*.js'],
+        dest: 'js/script.min.js'
+      }
+    },
+    */
+    watch: {// Compile everything into one task with Watch Plugin
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass', 'postcss', 'cssmin']
+      }//,
+      //js: {
+      //  files: '**/*.js',
+      //  tasks: ['uglify']
+      //}
+    }
+  });
+  // Load Grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-postcss')
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch')
+
+  // Register Grunt tasks
+  grunt.registerTask('default', ['watch'])
 };
