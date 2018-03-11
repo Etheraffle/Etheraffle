3340d997f9032daeaa66af31e1225a6cdbec54f7
Greg Kapka <greg@kapka.co.uk>
2018-03-11 00:29:19 +0000

    simplify getMissing init

--- eventwatcher/processes/getmissinginit.js
+++ eventwatcher/processes/getmissinginit.js
@@ -2,23 +2,18 @@ const {fork} = require('child_process'),
       utils  = require('../modules/utils')
 /*
 This method is called in the main eventwatcher.js file via a daily cron job. Can also be run when needed via node in this directory:
-$ const whatever = require('./getmissinginit')
-$ whatever.manualInit(args)
+> const whatever = require('./getmissinginit')
+> whatever.init(args)
 */
-/* To run this process via the command line per above... */
-const manualInit = function(_weekNo){
-  init(_weekNo, './getmissingprocess')//path relative to this file...
-}
-
 /* Already running processes call this function */
-const init = function (_weekNo, _path){//requires at least one entry already in db!
+const init = (_weekNo, _path) => {//requires at least one entry already in db!
   console.log("Week number according to function: ", utils.getWeekNo())
-  const raffleID = _weekNo == undefined ? utils.getWeekNo() : _weekNo
-  const getMissingEntries = _path == undefined ? fork("./processes/getmissingprocess") : fork(_path)
+  const raffleID = _weekNo == undefined ? utils.getWeekNo() : _weekNo,
+        getMissingEntries = _path == undefined ? fork("./getmissingprocess") : fork(_path)
   console.log("getMissingEntries Process Spawned on", utils.getTime())
   getMissingEntries.send(raffleID)
   getMissingEntries.on("message", msg => {
-    if(msg == "Complete!" || msg == "Errored!"){//process emails error reports...
+    if(msg == "Complete!" || msg == "Errored!") {
       getMissingEntries.kill()
       console.log("getMissingEntries process killed with status: ", msg, " on: ", utils.getTime())
     } else {
@@ -28,6 +23,5 @@ const init = function (_weekNo, _path){//requires at least one entry already in 
 }
 
 module.exports = {
-  init:       init,
-  manualInit: manualInit
+  init: init
 }
--- eventwatcher/processes/getwithdrawninit.js
+++ eventwatcher/processes/getwithdrawninit.js
@@ -5,14 +5,14 @@ This method is called in the main eventwatcher.js file via a daily cron job. Can
 > const whatever = require('./getmissingprocess')
 > whatever.init(args)
 */
-/* Init withdrawn process - defaults to 6 hour period & correct path)*/
+/* Init withdrawn process - defaults to 6 hour period & correct path */
 const init = function(_period, _path) {
   const period = _period == undefined ? 6 : _period,
         getWithdrawals = _path == undefined ? fork("./getwithdrawnprocess") : fork(_path)
   console.log("getWithdrawals Process Spawned on", utils.getTime())
   getWithdrawals.send(period)
   getWithdrawals.on("message", msg => {
-    if(msg == "Complete!" || msg == "Errored!"){//process itself emails error reports...
+    if(msg == "Complete!" || msg == "Errored!") {//process itself emails error reports...
       getWithdrawals.kill()
       console.log("getWithdrawals process killed with status: ", msg, " on: ", utils.getTime())
     } else {
