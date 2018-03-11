34c3e95db532d5733d131698a0e4b053aa5ffd10
Greg Kapka <greg@kapka.co.uk>
2018-03-11 00:21:40 +0000

    simplify the withdraw init method

--- eventwatcher/processes/getwithdrawninit.js
+++ eventwatcher/processes/getwithdrawninit.js
@@ -2,18 +2,13 @@ const {fork} = require('child_process'),
       utils  = require('../modules/utils')
 /*
 This method is called in the main eventwatcher.js file via a daily cron job. Can also be run when needed via node in this directory:
-$ const whatever = require('./getmissingprocess')
-$ whatever.manualInit(args)
+> const whatever = require('./getmissingprocess')
+> whatever.init(args)
 */
-/* To run this process via the command line per above (period (# of hours) can be ommitted safely) */
-const manualInit = function(_period) {
-  init(_period, './getwithdrawnprocess')//path relative to this file...
-}
-
-/* Already running processes call this function (Defaults to 6 hour period)*/
+/* Init withdrawn process - defaults to 6 hour period & correct path)*/
 const init = function(_period, _path) {
-  const period = _period == undefined ? 6 : _period
-  const getWithdrawals = _path == undefined ? fork("./getwithdrawnprocess") : fork(_path)
+  const period = _period == undefined ? 6 : _period,
+        getWithdrawals = _path == undefined ? fork("./getwithdrawnprocess") : fork(_path)
   console.log("getWithdrawals Process Spawned on", utils.getTime())
   getWithdrawals.send(period)
   getWithdrawals.on("message", msg => {
@@ -27,6 +22,5 @@ const init = function(_period, _path) {
 }
 
 module.exports = {
-  init:       init,
-  manualInit: manualInit
+  init: init
 }
