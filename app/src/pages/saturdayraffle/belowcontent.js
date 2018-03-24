54dae32b33899fa73d76cb8628ba7edf8decc5eb
Greg Kapka <greg@kapka.co.uk>
2018-03-24 16:19:27 +0000

    tweak sentence

--- app/src/pages/saturdayraffle/belowcontent.js
+++ app/src/pages/saturdayraffle/belowcontent.js
@@ -45,7 +45,7 @@ export default class BelowContent extends React.Component {
                 {/* Eth account unlocked and good to go... */}
                 {(window.ethAdd !== undefined && window.ethAdd !== null) &&
                   <p className="justify">
-                   Grab your ticket to enter the draw to be in with a chance to win ether! You win by matching three of more numbers. The timer counts down until the entry closes at 7pm UTC time every Saturday, and the results are drawn two hours later - <span className={'styledSpan + screen' + this.props.screenIndex}>Good Luck!</span> By playing Etheraffle you are not only in with a chance to win ether, but also are helping to support good causes around the world! <a className={'invert screen' + this.props.screenIndex} href='https://etheraffle.com/ico' target='_blank' rel='noopener noreferrer'>Want to know more?</a>
+                   Grab your ticket to enter the draw and be in with a chance to win ether! You win by matching three of more numbers. The timer counts down until the entry closes at 7pm UTC time every Saturday, and the results are drawn two hours later - <span className={'styledSpan + screen' + this.props.screenIndex}>Good Luck!</span> By playing Etheraffle you are not only in with a chance to win ether, but also are helping to support good causes around the world! <a className={'invert screen' + this.props.screenIndex} href='https://etheraffle.com/ico' target='_blank' rel='noopener noreferrer'>Want to know more?</a>
                   </p>
                 }
 
