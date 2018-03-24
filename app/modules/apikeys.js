41cf4062f8d0721bd249b048a0551c223ece3272
Greg Kapka <greg@kapka.co.uk>
2018-03-24 14:57:45 +0000

    fixed untracked files

--- app/modules/apikeys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongo:
-  "mongodb://etheraffle:0xd19b36eb8bbbf3154236a7bf311254f05b455ec254b50adf3f54a98cf8eb11b1@188.166.40.82/etheraffle?authSource=admin",
-  mailgun: "key-d5017d38aab88ce6535c17dd8a6ca2e7",
-  myDomain: "etheraffle.com"
-}
