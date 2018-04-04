9711e2cc0b03fa2d06b8b9709e08310ed49846d7
Greg Kapka <greg@kapka.co.uk>
2018-04-04 14:08:16 +0100

    move promo specific web3 file

--- app/src/pages/saturdayraffle/promo/web3/getNumEntries.js
+++ app/src/pages/saturdayraffle/promo/web3/getNumEntries.js
@@ -1,11 +1,11 @@
-import utils from '../components/utils'
-import getContInst from './getContInst'
+import utils from '../../../../components/utils'
+import getContInst from '../../../../web3/getContInst'
 /* Returns either the number of user's entries or rejects with err (weekNo defaults to current exact week) */
-export default (_which, _address, _weekNo = utils.getExactWeekNo()) => {
+export default (_address, _weekNo = utils.getExactWeekNo()) => {
   return new Promise((resolve, reject) => {
     if (window.web3 === null || window.web3.isConnected() === false)
       return reject(new Error("Failed to retrieve number of entries - no web3 connection!"))
-    return getContInst(_which)
+    return getContInst('Saturday')
     .then(etheraffle => {
       etheraffle.getUserNumEntries.call(_address, _weekNo, (err,res) => {
         return !err ? resolve(JSON.parse(res)) : reject(err)
