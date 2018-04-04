e9b8669aae64f264fd6da46890d3535b938afdf6
Greg Kapka <greg@kapka.co.uk>
2018-04-05 00:31:16 +0100

    clean out deprecated code

--- app/src/web3/getLowGas.js
+++ app/src/web3/getLowGas.js
@@ -13,20 +13,3 @@ export default () => {
     })
   })
 }
-/* Old version now deprecated...
-export default () => {
-  return new Promise((resolve, reject) => {
-    return fetch("https://etheraffle.com/api/gas")
-    .then(res => {
-      if (res.status !== 200) return reject(new Error('Cannot retrieve low gas prices!'))
-      return res.json()
-      .then(json => {
-        if (!(json.safeLow > 0)) return resolve(null)
-        let price = json.safeLow  + ' Gwei'
-        return resolve(price)
-      })
-    })
-    .catch(err => {return reject(null)})//We don't care about errors here
-  })
-}
-*/
