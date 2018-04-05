85a4cb283660c7bf98917a8d760aa3092f24bed9
Greg Kapka <greg@kapka.co.uk>
2018-04-05 12:52:58 +0100

    switch to function param defaults

--- eventwatcher/processes/getweeklyeventsinit.js
+++ eventwatcher/processes/getweeklyeventsinit.js
@@ -5,19 +5,18 @@ const {fork} = require('child_process')
  *        event watcher may have missed. Requires at least one entry to
  *        already be in the database for the raffle in question.
  * 
- * @param _period The period of days to look over.
+ * @param _period   The period of days to look over.
  * 
- * @param _path   The path to the js file the forked process runs. Defaults
- *                to correct path relative to this file.
+ * @param _path     The path to the js file the forked process runs. 
+ *                  Defaults to correct path relative to this file.
  * 
- * @param _weekNo The desired week number (defaults to current week)
+ * @param _weekNo   The desired week number - defaults to week prior  
+ *                  to the current week.
  */
-const init = (_period, _path, _weekNo) => {
-  const period   = _period == undefined ? 7 : _period,
-        getOrac  = _path   == undefined ? fork("./getweeklyeventsprocess") : fork(_path),
-        raffleID = _weekNo == undefined ? (utils.getWeekNo() - 1) : _weekNo
-  console.log(`getWeeklyEvents Process Spawned on ${utils.getTime()}`)
-  getOrac.send([raffleID, period])
+const init = (_period = 7, _path = './getweeklyeventsprocess', _weekNo = utils.getWeekNo() - 1) => {
+  const getOrac  = fork(_path)
+  console.log(`getWeeklyEvents Process Spawned on ${utils.getTime()} for week number: ${_weekNo}`)
+  getOrac.send([_weekNo, _period])
   getOrac.on("message", msg => {
     if (msg == "Complete!" || msg == "Errored!") {
       getOrac.kill()
