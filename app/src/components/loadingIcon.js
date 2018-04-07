f010605c4915f9c717388a3872841b4677367cfc
Greg Kapka <greg@kapka.co.uk>
2018-04-07 01:42:52 +0100

    simplify sfc

--- app/src/components/loadingIcon.js
+++ app/src/components/loadingIcon.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import icon from '../images/loadingIconGrey.svg'
 
-export default () => {
-  return(
-    <img className='loadingIcon' src={icon} alt='Loading Icon' />
-  )
-}
+export default () => (
+  <img className='loadingIcon' src={icon} alt='Loading Icon' />
+)
