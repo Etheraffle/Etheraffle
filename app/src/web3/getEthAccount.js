af70c958c5e09221474000db22927e84724c7167
Greg Kapka <greg@kapka.co.uk>
2018-04-07 17:02:19 +0100

    add timing element to cap loading time

--- app/src/web3/getEthAccount.js
+++ app/src/web3/getEthAccount.js
@@ -1,10 +1,20 @@
-/* Returns primary account or false if locked */
-export default _web3 => {
+/* Returns primary eth account or rejects with err */
+const getAccounts = _web3 => {
   return new Promise((resolve, reject) => {
+    /* 2 secs since getAccounts may not run if Metamask is locked */
+    setTimeout(() => {reject(new Error('Unable to retrieve ethereum account!'))}, 2000)
     _web3.eth.getAccounts((err,res) => {
       if (err) return reject(err)
       if (res.length !== 0 && res[0].length > 0) return resolve(res[0])
-      return resolve(false)// locked
+      return reject(new Error ("Ethereum account locked!"))
     })
+  })
+}
+/* Returns primary eth account or false */
+export default _web3 => {
+  return new Promise ((resolve, reject) => {
+    return getAccounts(_web3)
+    .then(res => resolve(res))
+    .catch(err => resolve(false))
   })
 }
