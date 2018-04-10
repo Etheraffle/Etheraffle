16f31608aa1fee9828bc4ad88efff4c883ed5b3b
Greg Kapka <greg@kapka.co.uk>
2018-04-10 01:07:44 +0100

    contract type now a var

--- app/src/web3/getContInst.js
+++ app/src/web3/getContInst.js
@@ -3,7 +3,7 @@ import etheraffleSatContract from './etheraffleSatContract'
 export default (_web3, _which) => {
   return new Promise((resolve, reject) => {
     let cont
-    if (_which === "Saturday") cont = etheraffleSatContract
+    if (_which === 'Saturday' || _which === 5) cont = etheraffleSatContract
     let cAdd  = cont.cAdd
       , cABI  = _web3.eth.contract(cont.ABI)
       , cInst = cABI.at(cAdd)
