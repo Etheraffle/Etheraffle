1ff05694ceec25c1c50688e5549f1ef10da0b2c4
Greg Kapka <greg@kapka.co.uk>
2018-04-10 01:06:03 +0100

    consuming screen context

--- app/src/components/footer.js
+++ app/src/components/footer.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import Socials from './socials'
-import { Link } from 'react-router'
+import { ScreenContext } from '../contexts/screenContext'
 
-export default props => {
-    return(
-        <div className="underInfo">
-            <p className='info'>
-            <b>&#x274d;</b> v 0.9.2 Beta <b>&#x274d;</b>
-            &nbsp;
-            <Link className={'routerLink screen' + props.screenIndex} to='/contact'>
-                Contact
-            </Link>
-            &nbsp;
-            <b>&#x274d;</b> © Etheraffle {(new Date()).getFullYear()} <b>&#x274d;</b>
-            </p>
-            <Socials screenIndex={props.screenIndex} />
-        </div>
-    )
-}
+export default props => (
+    <ScreenContext>
+        {screen => (
+            <div className="underInfo">
+                <p className='info'>
+                    <b>&#x274d;</b> v 0.9.2 Beta <b>&#x274d;</b>
+                    &nbsp;
+                    <a 
+                        className={`screen${screen.screenIndex}`} 
+                        style={{'cursor':'pointer'}} 
+                        onClick={screen.Contact}>
+                        Contact
+                    </a>
+                    &nbsp;
+                    <b>&#x274d;</b> © Etheraffle {(new Date()).getFullYear()} <b>&#x274d;</b>
+                </p>
+                <Socials screenIndex={screen.screenIndex} />
+            </div>
+        )}
+    </ScreenContext>
+)
