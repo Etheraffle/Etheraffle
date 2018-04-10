296371beab3ff8793d8705cf3321686d8d4098a4
Greg Kapka <greg@kapka.co.uk>
2018-04-10 01:10:56 +0100

    now requires web3 object

--- app/src/pages/saturdayraffle/promo/web3/getContInst.js
+++ app/src/pages/saturdayraffle/promo/web3/getContInst.js
@@ -1,10 +1,9 @@
 import cont from './promoContract'
-
 /* Returns the instantiated contract for use in transactions */
-export default () => {
+export default (_web3) => {
   return new Promise((resolve, reject) => {
     let cAdd  = cont.cAdd
-      , cABI  = window.web3.eth.contract(cont.ABI)
+      , cABI  = _web3.eth.contract(cont.ABI)
     return resolve(cABI.at(cAdd))
   })
 }
