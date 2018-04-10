ca8763e2bcf3a64b476d899b2ebdd6efb23088a3
Greg Kapka <greg@kapka.co.uk>
2018-04-10 01:09:34 +0100

    rm redundant cxn checks

--- app/src/pages/saturdayraffle/promo/web3/getNumEntries.js
+++ app/src/pages/saturdayraffle/promo/web3/getNumEntries.js
@@ -1,11 +1,9 @@
 import utils from '../../../../components/utils'
 import getContInst from '../../../../web3/getContInst'
 /* Returns either the number of user's entries or rejects with err (weekNo defaults to current exact week) */
-export default (_address, _weekNo = utils.getExactWeekNo()) => {
+export default (_web3, _address, _weekNo = utils.getExactWeekNo(), _which = 'Saturday') => {
   return new Promise((resolve, reject) => {
-    if (window.web3 === null || window.web3.isConnected() === false)
-      return reject(new Error("Failed to retrieve number of entries - no web3 connection!"))
-    return getContInst('Saturday')
+    return getContInst(_web3, _which)
     .then(etheraffle => {
       etheraffle.getUserNumEntries.call(_address, _weekNo, (err,res) => {
         return !err ? resolve(JSON.parse(res)) : reject(err)
