71d8e8c4c240ebbdbdddf902b5268cec76cb9428
Greg Kapka <greg@kapka.co.uk>
2018-04-10 01:08:25 +0100

    remove redundant cxn checks

--- app/src/web3/getPrizePool.js
+++ app/src/web3/getPrizePool.js
@@ -2,9 +2,7 @@ import getContInst from './getContInst'
 /* Returns either the prizePool or rejects with err */
 export default (_web3, _which) => {
   return new Promise((resolve, reject) => {
-    if (_web3 === null || _web3.isConnected() === false)
-      return reject(new Error("Failed to retrieve prizePool - no web3 connection!"))
-    return getContInst(_which)
+    return getContInst(_web3, _which)
     .then(etheraffle => {
       etheraffle.prizePool.call((err, res) => {
         return !err ? resolve(JSON.parse(res)) : reject(err)
--- app/src/web3/getTicketPrice.js
+++ app/src/web3/getTicketPrice.js
@@ -2,9 +2,7 @@ import getContInst from './getContInst'
 /* Returns either the ticket price or rejects with error */
 export default (_web3, _which) => {
   return new Promise((resolve, reject) => {
-    if (_web3 === null || _web3.isConnected() === false)
-      return reject(new Error("Failed to retrieve tktPrice - no web3 connection!"))
-    getContInst(_which)
+    getContInst(_web3, _which)
     .then(etheraffle => {
       etheraffle.tktPrice.call((err, res) => {
         return !err ? resolve(JSON.parse(res)) : reject(err)
