de83a0a78bfe40f41e791952462a92e7d6bb9aa9
Greg Kapka <greg@kapka.co.uk>
2018-04-12 00:58:45 +0100

    rejig modal to consume new contexts

--- app/src/components/modals/notconnectedinfo.js
+++ app/src/components/modals/notconnectedinfo.js
@@ -7,10 +7,10 @@ const Mobile = props => (
   <div className='info'>
     <img className='cipherLogo' src={cipher} alt='Cipher Browser Logo' />
       <p className='justify'>
-        <span className={"styledSpan screen" + props.screenIndex}>
+        <span className={"styledSpan screen" + props.sI}>
           On mobile?&nbsp;
         </span>
-        <a className={"screen" + props.screenIndex} target="_blank" rel="noopener noreferrer" href="https://www.cipherbrowser.com/">
+        <a className={"screen" + props.sI} target="_blank" rel="noopener noreferrer" href="https://www.cipherbrowser.com/">
           Cipher Browser is the world's first full-featured mobile dapp browser and wallet for the Ethereum blockchain. Click for more info.
         </a>
       </p>
@@ -22,10 +22,10 @@ const Desktop = props => (
     <div className='info'>
       <img className='metamaskLogo' src={metamask} alt='Metamask Logo' />
         <p className='justify'>
-          <span className={"styledSpan screen" + props.screenIndex}>
+          <span className={"styledSpan screen" + props.sI}>
             This is one to get you started!&nbsp;
           </span>
-          <a className={"screen" + props.screenIndex} target="_blank" rel="noopener noreferrer" href="https://metamask.io/">
+          <a className={"screen" + props.sI} target="_blank" rel="noopener noreferrer" href="https://metamask.io/">
             MetaMask is a bridge that allows you to visit the web of tomorrow in your browser today. It allows you to run ethereum ÐApps directly in your Chrome browser. Click for more info.
           </a>
         </p>
@@ -33,10 +33,10 @@ const Desktop = props => (
     <div className='info'>
       <img className='mistLogo' src={mist} alt='Mist Browser Logo' />
         <p className='justify'>
-          <span className={"styledSpan screen" + props.screenIndex}>
+          <span className={"styledSpan screen" + props.sI}>
             Want to try the browser of the future?&nbsp;
           </span>
-          <a className={"screen" + props.screenIndex} target="_blank" rel="noopener noreferrer" href="https://github.com/ethereum/mist/releases" >
+          <a className={"screen" + props.sI} target="_blank" rel="noopener noreferrer" href="https://github.com/ethereum/mist/releases" >
             The Mist Browser is a web-browser and ethereum wallet hybrid. It is specifically tailored for non-technical users and so a top choice for browsing and using ÐApps. Click for more info.
           </a>
         </p>
@@ -45,12 +45,12 @@ const Desktop = props => (
 )
 
 export default props => (
-  <div className={"modalNotConnectedInfo screen" + props.screenIndex}>
+  <div className={"modalNotConnectedInfo screen" + props.sI}>
     <p className='noCxn'>
-      <span className={"styledSpan screen" + props.screenIndex}>No ethereum connection detected!</span>
+      <span className={"styledSpan screen" + props.sI}>No ethereum connection detected!</span>
       <br/>
       Below are some options for getting connected to the ethereum network.
     </p>
-    {window.innerWidth > 800 ? <Desktop screenIndex={props.sreenIndex} /> : <Mobile screenIndex={props.sreenIndex} />}
+    {window.innerWidth > 800 ? <Desktop sI={props.sreenIndex} /> : <Mobile sI={props.sreenIndex} />}
   </div>
 )
