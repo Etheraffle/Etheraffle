b2c17cda4be991872710724908e77e00306ef50d
Greg Kapka <greg@kapka.co.uk>
2018-04-12 00:35:34 +0100

    alter cron scheduling and increase max listener count

--- eventwatcher/eventwatcher.js
+++ eventwatcher/eventwatcher.js
@@ -24,13 +24,11 @@ All process.exit points also send an email warning of what happened.
 */
 //process.on('warning', e => console.warn(e.stack))
 process.on('unhandledRejection', err => console.log('unhandledRejection', err.stack))//TODO: remove!
-require('events').EventEmitter.defaultMaxListeners = 15
-
+require('events').EventEmitter.defaultMaxListeners = 30
 /* Test for live eth connection */
 const block = web3.eth.getBlockNumber((err, res) => {
   return !err ? console.log(`Block: ${res} on ${utils.getTime()}`) : console.log(`Error retrieving block number: ${err}`)
 })
-
 /* The Event Watchers */
 ticketBoughtEvent.watch((err, res) => {
   if (err) return utils.errorHandler("ticketBoughtEvent", "eventwatcher", res, err), process.exit(1)
@@ -53,18 +51,18 @@ prizePoolsEvent.watch((err, res) => {//TODO: what is this function for?
   return prizePools(res)
 })
 /* Cron Jobs */
-/* Retrieves any missing entries once a day @ 5:30 am */
-cron.schedule('30 5 * * *', () => {
+/* Retrieves any missing entries every hour */
+cron.schedule('0 */1 * * *', () => {
   console.log(`Cron: getMissingProcess() Begun on ${utils.getTime()}`)
   return getMissing.init("./etheraffle/eventwatcher/processes/getmissingprocess")
 })
-/* Retrieves any manual withdrawals, runs every 3 hours */
-cron.schedule('0 */3 * * *', () => {
+/* Retrieves any manual withdrawals, runs every 3 hours (@ half past the hour) */
+cron.schedule('30 */3 * * *', () => {
   console.log(`Cron: getWithdrawnProcess() Begun on ${utils.getTime()}`)
   return getWithdrawn.init(6,'./etheraffle/eventwatcher/processes/getwithdrawnprocess')
 })
-/* Retrieves any weekly events, runs 5am Sunday morning. */
-cron.schedule('0 5 * * 0', () => {
+/* Retrieves any weekly events, runs 1am Sunday morning. */
+cron.schedule('0 1 * * 0', () => {
   console.log(`Cron: getOraclizeEventsProcess() Begun on ${utils.getTime()}`)
   return getOrac.init(7, "./etheraffle/eventwatcher/processes/getweeklyeventsprocess")
 })
