5dccebf916bf6a52073264a8cce07e5f5e8f6877
Greg Kapka <greg@kapka.co.uk>
2018-04-12 00:49:02 +0100

    switch from event emitters to context for nav

--- app/src/components/nav.js
+++ app/src/components/nav.js
@@ -1,47 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { ScreenContext } from '../contexts/screenContext'
 
-export default props => {
-  return(
-    <div className="app-nav">
-      
-      <div className={props.screenIndex === 1 ? "nav-item screen1 active-nav" : "nav-item screen1"}>
-        <Link to="/instant" style={{color: 'white'}}>
-          <p className="invisible">
-            <b>SCREEEEEEN1</b>
-          </p>
-        </Link>
+export default props => (
+  <ScreenContext>
+    {screen => (
+      <div className='app-nav'>
+        <div className={screen.screenIndex === 1 ? 'nav-item screen1 active-nav' : 'nav-item screen1'} onClick={screen.Instant} />
+        <div className={screen.screenIndex === 3 ? 'nav-item screen3 active-nav' : 'nav-item screen3'} onClick={screen.Wednesday} />
+        <div className={screen.screenIndex === 5 ? 'nav-item screen5 active-nav' : 'nav-item screen5'} onClick={screen.Saturday} />
+        <div className={screen.screenIndex === 2 ? 'nav-item screen2 active-nav' : 'nav-item screen2'}>
+          <a href='https://www.etheraffle.com/ico' target={window.innerWidth > 1000 ? '_blank' : ''} rel='noopener noreferrer' >
+            <p className='invisible'>
+              SCREEEEEEN2
+            </p>
+          </a>
+        </div>
       </div>
-
-      <div className={props.screenIndex === 3 ? "nav-item screen3 active-nav" : "nav-item screen3"}>
-        <Link to="/wednesday" style={{color: 'white'}}>
-          <p className="invisible">
-            <b>SCREEEEEEN3</b>
-          </p>
-        </Link>
-      </div>
-
-      <div className={props.screenIndex === 5 ? "nav-item screen5 active-nav" : "nav-item screen5"}>
-        <Link to="/saturday" style={{color: 'white'}}>
-          <p className="invisible">
-            <b>SCREEEEEEN5</b>
-          </p>
-        </Link>
-      </div>
-
-      <div className={props.screenIndex === 2 ? "nav-item screen2 active-nav" : "nav-item screen2"}>
-        <a
-          href='https://www.etheraffle.com/ico'
-          style={{color: 'white'}}
-          target={window.innerWidth > 1000 ? '_blank' : ''}
-          rel='noopener noreferrer'
-          >
-          <p className="invisible">
-            <b>SCREEEEEEN2</b>
-          </p>
-        </a>
-      </div>
-
-    </div>
-  )
-}
+    )}
+  </ScreenContext>
+)
