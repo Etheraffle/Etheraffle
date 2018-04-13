b877a830ea551efb232dfef0dc39034608e0751c
Greg Kapka <greg@kapka.co.uk>
2018-04-13 23:26:47 +0100

    rm redundant return

--- app/src/pages/faq/questions/CanIEnterMoreThanOnce.js
+++ app/src/pages/faq/questions/CanIEnterMoreThanOnce.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import multiple from './images/multiplewins.jpg'
 
-export default () => {
-  return(
-    <div>
-      <p className="justify">
-        Absolutely! You can enter any raffle as many times as you'd like! The more times you enter, the more chances you have of winning! You could even win with more than one ticket in the same draw:
-      </p>
-      <img className='image border centred' src={multiple} alt='Table showing multiple wins'/>
-    </div>
-  )
-}
+export default () => (
+  <div>
+    <p className="justify">
+      Absolutely! You can enter any raffle as many times as you'd like! The more times you enter, the more chances you have of winning! You could even win with more than one ticket in the same draw:
+    </p>
+    <img className='image border centred' src={multiple} alt='Table showing multiple wins'/>
+  </div>
+)
