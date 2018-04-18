a34d5159ae27f181e7ee8df0a5a616770307d85a
Greg Kapka <greg@kapka.co.uk>
2018-04-18 16:40:45 +0100

    auto center loading icon with inline css

---
+++ app/src/components/loading.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import icon from '../images/loading_icon_grey.svg'
+
+export default () => (
+  <img className='loadingIcon' src={icon} style={{'display':'flex','margin':'1em auto 1em auto'}} alt='Loading Icon' />
+)
