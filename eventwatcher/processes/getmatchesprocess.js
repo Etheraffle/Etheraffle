8cc47659addddb2af033634ed0ba6ebe579731a9
Greg Kapka <greg@kapka.co.uk>
2018-04-23 00:24:22 +0100

    clear out unit test comments

--- eventwatcher/processes/getmatchesprocess.js
+++ eventwatcher/processes/getmatchesprocess.js
@@ -167,9 +167,3 @@ const getWinningAmounts = (_matches, _prizePool) => {
     })
   }).catch(err => utils.errorHandler('getWinningAmounts', 'get_matches_process', `matchesArr: ${_matches} & prizePool: ${_prizePool}`, err))
 }
-  // Psuedo Unit tests:
-  // getWinningAmounts([0,0,0,1,0,0,0],2000000000000000000).then(res => console.log(res))
-  // = payouts arr: [0,0,0,168000000000000000,0,0,0], total: 168000000000000000 - CORRECT! (using odds not splits!)
-  //
-  // getWinningAmounts([0,0,0,10,5,2,1],1000000000000000000).then(res => console.log(res))
-  // = payOuts arr: [0,0,0,52000000000000000,22800000000000000,23500000000000000,319000000000000000], total: 1000000000000000000 - CORRECT (using splits not odds)
