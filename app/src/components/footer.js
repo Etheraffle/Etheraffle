4587fa3e493e0c4f3dc8c57c3db8fc54b41abce0
Greg Kapka <greg@kapka.co.uk>
2018-04-25 16:31:14 +0100

    fix indentation

--- app/src/components/footer.js
+++ app/src/components/footer.js
@@ -3,23 +3,23 @@ import Socials from './socials'
 import { ScreenContext } from '../contexts/screen_context'
 
 export default props => (
-    <ScreenContext>
-        {screen => (
-            <div className="underInfo">
-                <p className='info'>
-                    <b>&#x274d;</b> v 0.9.3 Beta <b>&#x274d;</b>
-                    &nbsp;
-                    <a 
-                        className={`screen${screen.screenIndex}`} 
-                        style={{'cursor':'pointer'}} 
-                        onClick={screen.Contact}>
-                        Contact
-                    </a>
-                    &nbsp;
-                    <b>&#x274d;</b> © Etheraffle {(new Date()).getFullYear()} <b>&#x274d;</b>
-                </p>
-                <Socials screenIndex={screen.screenIndex} />
-            </div>
-        )}
-    </ScreenContext>
+  <ScreenContext>
+    {screen => (
+      <div className="underInfo">
+        <p className='info'>
+          <b>&#x274d;</b> v 0.9.3 Beta <b>&#x274d;</b>
+          &nbsp;
+          <a 
+            className={`screen${screen.screenIndex}`} 
+            style={{'cursor':'pointer'}} 
+            onClick={screen.Contact}>
+            Contact
+          </a>
+          &nbsp;
+          <b>&#x274d;</b> © Etheraffle {(new Date()).getFullYear()} <b>&#x274d;</b>
+        </p>
+        <Socials screenIndex={screen.screenIndex} />
+      </div>
+    )}
+  </ScreenContext>
 )
