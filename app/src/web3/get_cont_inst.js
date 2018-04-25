7e187b37101427b068feb4f1c081e31f7a91adc0
Greg Kapka <greg@kapka.co.uk>
2018-04-25 16:31:56 +0100

    add freeLOT contract instantiation

--- app/src/web3/get_cont_inst.js
+++ app/src/web3/get_cont_inst.js
@@ -1,13 +1,14 @@
 import utils from '../components/utils'
+import freeCont from './free_lot_contract'
 import satCont from './etheraffle_sat_contract'
 /* Returns the instantiated contract for use in transactions */
 export default (_web3, _which, _weekNo = utils.getExactWeekNo()) => {
   return new Promise((resolve, reject) => {
     let cont
+    if (_which === 'Free') cont = freeCont()
     if (_which === 'Saturday' || _which === 5) cont = satCont(_weekNo)
     let cAdd  = cont.cAdd
       , cABI  = _web3.eth.contract(cont.ABI)
-      , cInst = cABI.at(cAdd)
-    return resolve(cInst)
+    return resolve(cABI.at(cAdd))
   })
 }
