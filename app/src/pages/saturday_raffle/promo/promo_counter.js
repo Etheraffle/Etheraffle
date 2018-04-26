41f051eca290aeb00f542a248cea0093a0aa635d
Greg Kapka <greg@kapka.co.uk>
2018-04-26 23:54:24 +0100

    fix indentation

--- app/src/pages/saturday_raffle/promo/promo_counter.js
+++ app/src/pages/saturday_raffle/promo/promo_counter.js
@@ -2,8 +2,8 @@ import React from 'react'
 import LOT from '../../../components/lot'
 
 export default props => (                       
-    <div className='promoCounter'>
-        <LOT height='3.5em' fill='6'/>
-        {props.children}
-    </div>
+  <div className='promoCounter'>
+    <LOT height='3.5em' fill='6'/>
+    {props.children}
+  </div>
 )
