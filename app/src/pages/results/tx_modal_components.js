1b1efa0ec70936da1f34631ed9ba2cca23e239e9
Greg Kapka <greg@kapka.co.uk>
2018-04-28 15:49:51 +0100

    pull out common results table components

--- app/src/pages/results/tx_modal_components.js
+++ app/src/pages/results/tx_modal_components.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import LoadingIcon from '../../images/loading_icon_grey.svg'
-import { ScreenContext } from '../../contexts/screen_context'
 
 export const Pending = props => (
   <React.Fragment>
@@ -16,32 +15,28 @@ export const Pending = props => (
 )
 
 export const Success = props => (
-  <ScreenContext.Consumer>
-    {({FAQ}) => (
-      <React.Fragment>
-        <h2 className={`screen${props.screenIndex}`}>Transaction Sent!</h2>
-        <p className='centred'>
-          Your transaction hash: 
-          <a
-            rel='noopener noreferrer'
-            target='_blank'
-            className={`invert screen${props.screenIndex}`}
-            href={`https://etherscan.io/tx/${props.txHash}`}>
-              {` ${props.txHash.substring(0, 20)}. . .`}
-          </a><br/>
-        </p>
-        <h2 className={`screen${props.screenIndex}`}>What now?</h2>
-        {props.freeGo
-          ? <p className='justify'>
-              Click the hash above to watch your transaction being mined in to the block chain! When your transaction is received by Etheraffle's smart-contract it will send you a <span className={`styledSpan screen${props.screenIndex}`}>FreeLOT</span> coupon! You can use this to enter any future raffle for free! For more information, <span className={`styledSpan screen${props.screenIndex}`} style={{'cursor':'pointer'}} onClick={FAQ} >see the FAQ</span>.<br/><br/>Please be patient whilst this is occuring. If after 24 hours you have not received your coupon, please contact support quoting your ethereum address and this transaction hash.
-            </p>
-          : <p className='justify'>
-              Click the hash above to watch your transaction being mined in to the block chain! When your transaction is received by Etheraffle's smart-contract it will send your ETH directly into your account!<br/><br/>Please be patient whilst this is occuring. If after 24 hours you have not received your ETH, please contact support quoting your ethereum address and this transaction hash.
-            </p>
-        }
-      </React.Fragment>
-    )}
-  </ScreenContext.Consumer>
+  <React.Fragment>
+    <h2 className={`screen${props.screenIndex}`}>Transaction Sent!</h2>
+    <p className='centred'>
+      Your transaction hash: 
+      <a
+        rel='noopener noreferrer'
+        target='_blank'
+        className={`invert screen${props.screenIndex}`}
+        href={`https://etherscan.io/tx/${props.txHash}`}>
+          {` ${props.txHash.substring(0, 20)}. . .`}
+      </a><br/>
+    </p>
+    <h2 className={`screen${props.screenIndex}`}>What now?</h2><br/>
+      {props.freeGo
+        ? <p className='justify'>
+            Click the hash above to watch your transaction being mined in to the block chain! When your transaction is received by Etheraffle's smart-contract it will send you a <span className={`styledSpan screen${props.screenIndex}`}>FreeLOT</span> coupon! You can use this to enter any future raffle for free! For more information about how to use your coupon, see the FAQ. If after 24 hours you have not received your coupon, please contact support quoting your ethereum address and the transaction hash above.
+          </p>
+        : <p className='justify'>
+            Click the hash above to watch your transaction being mined in to the block chain! When your transaction is received by Etheraffle's smart-contract it will send your ETH directly into your account! If after 24 hours you have not received your ETH, please contact support quoting your ethereum address and the transaction hash above.
+          </p>
+      }
+  </React.Fragment>
 )
 
 export const Error = props => (
