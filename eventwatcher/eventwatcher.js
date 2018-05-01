97d95286f3fbfcf33d241372d8909112251c7b44
Greg Kapka <greg@kapka.co.uk>
2018-05-01 21:30:12 +0100

    switch to single quotes

--- eventwatcher/eventwatcher.js
+++ eventwatcher/eventwatcher.js
@@ -23,7 +23,7 @@ So if needs be, function them out to their own file!
 All process.exit points also send an email warning of what happened.
 */
 //process.on('warning', e => console.warn(e.stack))
-process.on('unhandledRejection', err => console.log('unhandledRejection', err.stack))//TODO: remove!
+process.on('unhandledRejection', err => console.log('unhandledRejection', err.stack)) // TODO: remove!
 require('events').EventEmitter.defaultMaxListeners = 30
 /* Test for live eth connection */
 const block = web3.eth.getBlockNumber((err, res) => {
@@ -31,30 +31,30 @@ const block = web3.eth.getBlockNumber((err, res) => {
 })
 /* The Event Watchers */
 ticketBoughtEvent.watch((err, res) => {
-  if (err) return utils.errorHandler("ticketBoughtEvent", "eventwatcher", res, err), process.exit(1)
+  if (err) return utils.errorHandler('ticketBoughtEvent', 'eventwatcher', res, err), process.exit(1)
   return ticketBought(res)
 })
 winningNumbersEvent.watch((err, res) => {
-  if (err) return utils.errorHandler("winningNumbersEvent", "API", "None", err), process.exit(1)
+  if (err) return utils.errorHandler('winningNumbersEvent', 'API', 'None', err), process.exit(1)
   return winningNums(res)
 })
 withdrawEvent.watch((err, res) => {
-  if (err) return utils.errorHandler("withdrawEvent", "API", "None", err), process.exit(1)
+  if (err) return utils.errorHandler('withdrawEvent', 'API', 'None', err), process.exit(1)
   return withdrawal(res)
 })
 functionsPaused.watch((err, res) => {
-  if (err) return utils.errorHandler("functionsPaused", "API", "None", err), process.exit(1)
+  if (err) return utils.errorHandler('functionsPaused', 'API', 'None', err), process.exit(1)
   return paused(res)
 })
 prizePoolsEvent.watch((err, res) => {//TODO: what is this function for?
-  if (err) return utils.errorHandler("prizePoolsEvent", "API", "None", err), process.exit(1)
+  if (err) return utils.errorHandler('prizePoolsEvent', 'API', 'None', err), process.exit(1)
   return prizePools(res)
 })
 /* Cron Jobs */
 /* Retrieves any missing entries, runs every 10 minutes starting at 0 mins past the hour */
 cron.schedule('0-59/10 * * * *', () => {
   console.log(`Cron: getMissingProcess() Begun on ${utils.getTime()}`)
-  return getMissing.init("./etheraffle/eventwatcher/processes/getmissingprocess")
+  return getMissing.init('./etheraffle/eventwatcher/processes/getmissingprocess')
 })
 /* Retrieves any manual withdrawals, runs every 10 minutes starting at 5 mins past the hour */
 cron.schedule('5-59/10 * * * *', () => {
@@ -64,5 +64,5 @@ cron.schedule('5-59/10 * * * *', () => {
 /* Retrieves any weekly events, runs 1am Sunday morning. */
 cron.schedule('0 1 * * 0', () => {
   console.log(`Cron: getOraclizeEventsProcess() Begun on ${utils.getTime()}`)
-  return getOrac.init(7, "./etheraffle/eventwatcher/processes/getweeklyeventsprocess")
+  return getOrac.init(7, './etheraffle/eventwatcher/processes/getweeklyeventsprocess')
 })
